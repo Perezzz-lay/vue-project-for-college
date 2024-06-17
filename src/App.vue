78bd6d82ff35e4a590f73455edf9c39b15210b92
Perezzz-lay <75630097+Perezzz-lay@users.noreply.github.com>
2024-06-17 16:03:45 +0500

    Start 2 Добавление шрифтов

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <script>
 export default {
   data() {
-  	return {
-	    awesome: true
-  	}
-	}
-}
+    return {
+      awesome: true,
+      page: true
+    };
+  },
+  methods: {}
+};
 </script>
 
 <template>
-
+	<header v-if="page == true">
+		<div class="but-block">
+			<button>О нас</button>
+		</div>
+		<div class="but-block">
+			<button>Где мы</button>
+		</div>
+		<img src="/src/assets/logo.svg" alt="" width="100px;" height="100px" />
+		<div class="but-block">
+			<button>Связь с нами</button>
+		</div>
+		<div class="but-block">
+			<button>Наше ПО</button>
+		</div>
+	</header>
 </template>
 
-<style scoped>
-
+<style>
+@import url("/src/assets/style.css");
 </style>
