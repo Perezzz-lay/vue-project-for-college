734e1e2978566ec61d2cdb284036e9b4741da457
Perezzz-lay <75630097+Perezzz-lay@users.noreply.github.com>
2024-06-18 20:56:56 +0500

    Изменение 2

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,23 @@ export default {
   data() {
     return {
       awesome: true,
-      page: true
+      page: false,
+	  heads: true
     };
   },
   methods: {}
 };
 </script>
 
-<template>
-	<header v-if="page == true">
+<template v-if="page == true">
+	<header v-if="heads == true">
 		<div class="but-block">
 			<button>О нас</button>
 		</div>
 		<div class="but-block">
 			<button>Где мы</button>
 		</div>
-		<img src="/src/assets/logo.svg" alt="" width="100px;" height="100px" />
+		<img src="/src/assets/logo.svg" alt="" width="100%;" height="100px" />
 		<div class="but-block">
 			<button>Связь с нами</button>
 		</div>
@@ -26,6 +27,14 @@ export default {
 			<button>Наше ПО</button>
 		</div>
 	</header>
+	<div class="block-start">
+		<div class="texts">
+		<h1 class="First">АлгаРитм</h1>
+		<h3 class="Third">Технологии, которые работают</h3>
+		</div>
+		<img src="/src/assets/ноуты1.jpg" alt="ноутбуки" >
+	</div>
+	<hr>
 </template>
 
 <style>
